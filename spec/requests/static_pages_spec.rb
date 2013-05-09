8f5a646ffba7132a14bd5c5086e9c29b4fd05aa5
MavizZafar <maviz.zafar@nxb.com.pk>
2013-05-09 17:23:04 +0500

    first commit 9may2013

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -3,37 +3,28 @@ require 'spec_helper'
 
 describe "Static pages" do
   let(:base_title) { "Ruby on Rails Tutorial Sample App | " }
+  subject { page }
   describe "Home page" do
-    it "should have the content 'Sample application'" do
-     visit 'http://0.0.0.0:3000/static_pages/home'
-     page.should have_content('Sample application')
-    end
-    it "should have the right title" do 
- 	visit "/static_pages/home"
- 	page.should have_selector('title', :text => "#{base_title}Home")
-    end
+    before { visit root_path}
+    it { should have_selector('title', text: "#{base_title}Home") }
+     it { should have_selector('h1', text: "Sample Application") }  
   end
 
  describe "Help page" do 
- 	it "should have the content 'help'" do 
- 		visit 'http://0.0.0.0:3000/static_pages/help'
- 		page.should have_content('help')
- 	end
- 	it "should have the right title" do 
- 	visit "/static_pages/help"
- 	page.should have_selector('title', :text => "#{base_title}Help")
-    end 
+  before { visit help_path }
+ 	it { should have_selector('title', text: "#{base_title}Help")}
+  it {should have_selector('h1', text: "help")}
 end
- describe "About page" do 
- 	it "shoyuld have the content 'About Us'" do 
- 		visit 'http://0.0.0.0:3000/static_pages/about'
- 		page.should have_content('About Us')
- 	end 
- 	it "should have the right title" do 
- 	visit "/static_pages/about"
- 	page.should have_selector('title', :text => "#{base_title}About Us")
-    end
- end
-   
 
+ describe "About page" do 
+  before { visit about_path}
+  it { should have_selector('title', text: "#{base_title}About")}
+  it {should have_selector('h1', text: "About Us")}
+ end
+
+ describe "Contact page" do 
+   before { visit contact_path}
+   it {should have_selector('title', text: "#{base_title}Contact")}
+   it {should have_selector('h1'), text: "Contact"}
+  end
 end
